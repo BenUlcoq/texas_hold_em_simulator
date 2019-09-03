877fe2a1657eda2645830057770a66e722a1454e
Ben Ulcoq <ben.ulcoq@gmail.com>
2019-09-03 16:03:24 +1000

    Implemented Community Cards, restructured code for improved efficiency and readability

--- player.rb
+++ player.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 
 class Player
-    attr_reader :player_name
-    attr_accessor :hole_cards
-    def initialize(name)
-        @player_name = name
-        @hole_cards = []
-    end
+  attr_reader :player_name
+  attr_accessor :hole_cards
+  def initialize(name)
+    @player_name = name
+    @hole_cards = []
+  end
 end
--- tests.rb
+++ tests.rb
@@ -1,23 +1,37 @@
+# frozen_string_literal: true
+
 require 'test/unit'
 require_relative 'poker'
 
 class PokerTest < Test::Unit::TestCase
-    # Create a new Customer and check if it's first_name is correct.
+  # Create a new Customer and check if it's first_name is correct.
 
-    def setup
-        @poker = Poker.new(8, 'Ben Ulcoq', 'Robert Downey')
-    end
+  def setup
+    @poker = Poker.new(8, 'Ben Ulcoq', 'Robert Downey')
+  end
 
-    # def test_players
-    #     assert_equal(8, @poker.table.player_positions.length)
-    # end
+  #   def test_players
+  #       assert_equal(8, @poker.player_positions.length)
+  #   end
 
-    # def test_args
-    #     assert_equal('Ben Ulcoq', @poker.table.player_positions[0].player_name)
-    #     assert_equal('Robert Downey', @poker.table.player_positions[1].player_name)
-    # end
+  #   def test_args
+  #       assert_equal('Ben Ulcoq', @poker.player_positions[0].player_name)
+  #       assert_equal('Robert Downey', @poker.player_positions[1].player_name)
+  #   end
 
-    def test_hole_cards
-        assert_equal(['2C', '4C'], @poker.table.player_positions[0].hole_cards)
-    end
+  #   def test_hole_cards
+  #       assert_equal(['2C', '4C'], @poker.player_positions[0].hole_cards)
+  #   end
+
+  # def test_flop
+  #     assert_equal(['6H', '6S', '6D'], @poker.community_cards)
+  # end
+
+  # def test_turn
+  #     assert_equal(['6H', '6S', '6D', '7H'], @poker.community_cards)
+  # end
+
+  def test_river
+    assert_equal(%w[6H 6S 6D 7H 7D], @poker.community_cards)
+  end
 end
